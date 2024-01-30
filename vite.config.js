bb08b83fe19821dd679fa50751a6a70150eef485
twestaus <127304670+twestaus@users.noreply.github.com>
2024-01-31 07:35:20 +1000

    update template files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import htmlPurge from 'vite-plugin-purgecss';
 
 export default defineConfig({
   root: resolve(__dirname, "src/"),
+  plugins: [
+    htmlPurge(),
+  ],
   build: {
     outDir: "../dist",
     emptyOutDir: true,
